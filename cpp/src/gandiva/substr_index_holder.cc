52e7a8071ee3da040dcbb808d132785d4930e4c8
philo <feilong.he@intel.com>
2022-05-07 14:31:37 +0800

    Implement substr_index in a func. holder

---
+++ cpp/src/gandiva/substr_index_holder.cc
@@ -0,0 +1,101 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "gandiva/substr_index_holder.h"
+
+#include <string>
+#include "gandiva/node.h"
+
+namespace gandiva {
+
+Status SubStrIndexHolder::Make(const FunctionNode& node, std::shared_ptr<SubStrIndexHolder>* holder) {
+  return Make(holder);
+}
+
+Status SubStrIndexHolder::Make(std::shared_ptr<SubStrIndexHolder>* holder) {
+  *holder = std::shared_ptr<SubStrIndexHolder>(new SubStrIndexHolder());
+  return Status::OK();
+}
+
+const uint8_t* SubStrIndexHolder::operator()(gandiva::ExecutionContext* ctx, std::string input_str, 
+                          std::string delim, int count, int32_t* out_len) {
+  // No need to find delim when count = 0 and just return "".
+  if (count == 0) {
+    out_len = 0;
+    return "";
+  }
+
+  std::size_t index;
+  if (count > 0) {
+    int n = 0;
+    index = 0;
+    while (n++ < count) {
+      index = input_str.find(delim, index);
+      if (index == std::string::npos) {
+        break;
+      }
+      if (n < count) {
+        index++;
+      }
+    }
+  } else {
+    int n = 0;
+    index = in_len - 1;
+    while (n++ < -count) {
+      index = input_str.rfind(delim, index);
+      if (index == std::string::npos) {
+        break;
+      }
+      if (n < -count) {
+        index--;
+      }
+    }
+  }
+
+  if (index == std::string::npos) {
+    *out_len = static_cast<int32_t>(input_str.length());
+    char* result_buffer = reinterpret_cast<char*>(gdv_fn_context_arena_malloc(ctx, *out_len));
+    if (result_buffer == NULLPTR) {
+      gdv_fn_context_set_error_msg(ctx, "Could not allocate memory for result");
+      *out_len = 0;
+      return "";
+    }
+    memcpy(result_buffer, input_str.data(), *out_len);
+    return result_buffer;
+  }
+
+  std::string result;
+  if (count > 0) {
+    *out_len = static_cast<int32_t>(index);
+    result = input_str.substr(0, *out_len);
+  } else {
+    // Delimiter length can be greater than 1,
+    *out_len = static_cast<int32_t>(input_str.length() - index - delim_len);
+    result = input_str.substr(index + delim_len, *out_len);
+  }
+
+  char* result_buffer = reinterpret_cast<char*>(gdv_fn_context_arena_malloc(ctx, *out_len));
+  if (result_buffer == NULLPTR) {
+    gdv_fn_context_set_error_msg(ctx, "Could not allocate memory for result");
+    *out_len = 0;
+    return "";
+  }
+  memcpy(result_buffer, result.data(), *out_len);
+  return result_buffer;
+}
+
+} // namespace gandiva
