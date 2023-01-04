f33c102558b3b194a631b2833eb781d7616af159
PHILO-HE <feilong.he@intel.com>
2023-01-04 23:14:28 +0800

    Initial commit

--- cpp/src/gandiva/json_holder.cc
+++ cpp/src/gandiva/json_holder.cc
@@ -150,6 +150,10 @@ const uint8_t* JsonHolder::operator()(gandiva::ExecutionContext* ctx, const std:
     }
   }
   formatted_json_path[j] = '\0';
+  auto ret_code =  doc.find_field(formatted_json_path);
+  if (ret_code == error_code::NO_SUCH_FIELD) {
+    return nullptr;
+  }
 
   std::string res;
   error_code error;
